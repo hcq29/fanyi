6493ea128adddde76ec8787e9313e3347f70ad8f
hcq29 <2581143034@qq.com>
2019-12-31 00:58:43 +0800

    简易版在线翻译

---
+++ src/components/TranslateOutput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="translateOutput">
+    <h1>{{translatedText}}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  props: [
+      "translatedText"
+  ],
+  components: {
+  }
+}
+</script>
+
+<style>
+#translateOutput {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
